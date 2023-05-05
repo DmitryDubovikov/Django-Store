9931a5d6fe7ce1a65e5f81aaa0be8854f12c1763
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-05-05 19:20:34 -0300

    products app, static files, index and products pages

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # external
+    # internal apps
+    "products.apps.ProductsConfig",
 ]
 
 MIDDLEWARE = [
@@ -121,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
