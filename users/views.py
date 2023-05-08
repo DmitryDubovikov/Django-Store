b5b0631ce46c3a9af2544609f6e6a7a0e67994dc
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-05-08 11:18:44 -0300

    SuccessMessageMixin, TitleMixin

--- users/views.py
+++ users/views.py
@@ -5,31 +5,34 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
+from common.views import TitleMixin
 
 
-class UserLoginView(LoginView):
+class UserLoginView(TitleMixin, LoginView):
     template_name = "users/login.html"
     form_class = UserLoginForm
+    title = "Store - Authorization"
 
 
-class UserRegistrationView(CreateView):
+class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
     model = User
     form_class = UserRegistrationForm
     template_name = "users/registration.html"
     success_url = reverse_lazy("users:login")
-    # success_message = "Вы успешно зарегестрированы!"
-    title = "Store - Регистрация"
+    success_message = "You are succesfully registered!"
+    title = "Store - Registration"
 
 
-class UserProfileView(UpdateView):
+class UserProfileView(TitleMixin, UpdateView):
     model = User
     form_class = UserProfileForm
     template_name = "users/profile.html"
-    # title = 'Store - Личный кабинет'
+    title = "Store - profile"
 
     def get_success_url(self):
         return reverse_lazy("users:profile", args=(self.object.id,))
